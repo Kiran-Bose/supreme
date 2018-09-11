c1fbdd22caaa6bedb9b14196baafe0c1328e9736
Kiran-Bose <kiran.vkvr@gmail.com>
2018-09-11 11:53:52 +0530

    Remove

--- supreme-1.0/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(supreme):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="supreme" command="/usr/bin/supreme"
--- supreme-1.0/debian/supreme.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the supreme package
-#
-0 4	* * *	root	[ -x /usr/bin/supreme_maintenance ] && /usr/bin/supreme_maintenance
